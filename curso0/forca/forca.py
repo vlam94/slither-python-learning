698ec46b509c4a464767e065ebce1822848e00e5
Rabico <vlam94@gmail.com>
2023-01-09 18:24:37 -0300

    melhorando as funcoes

--- curso0/forca/forca.py
+++ curso0/forca/forca.py
@@ -212,8 +212,8 @@ FORCAIMG= ['''
 ''']
 chutes=[]
 palavras=str() #atualizar com novas palavras usando .append e quando sair reescrever palavras.txt a partir do palavras[]
-#with open('cursos/curso0/forca/palavras.txt') as f: #vscode
-with open("palavras.txt") as f: #terminal
+with open('slither-python-learning/curso0/forca/palavras.txt') as f: #vscode
+#with open("palavras.txt") as f: #terminal
     for line in f:
         palavras+=line.strip().upper()
         palavras+=' '
@@ -223,7 +223,7 @@ with open("palavras.txt") as f: #terminal
 def escolhePalavra():
     global palavras
     while True:
-        plve=random.choice
+        plve=random.choice(palavras)
         if len(plve)>2:
             return plve
 
@@ -238,6 +238,8 @@ def chuta():
     global chutes
     while True: #erros=True
         chute=input("Chute uma letra:\n")
+        if chute=="exit":
+            bye()
         if not chute:
             continue
         chute=chute[0].upper()            
@@ -319,6 +321,102 @@ def addPalavra():
         plv = input("insira palavra sem 'ç' e acentos,\nou digite * para sair do editor: ").upper()
     f.close()
     return
+
+def winPrint(win):
+    global palavras, pos
+    if win:
+        palavras.remove(forca[0]) #retira reocorrencia de palavras
+        print (u'''
+
+            
+**************
+*  PARABÉNS  *
+*VOCÊ ACERTOU*
+**************
+            
+            ''')
+        if pos==0: # ***o elif meio q "soma"(and) "condições negativas" pro else, é isso?*** #viagens -tinha um elif aqui :(
+            print("*1#1#1#1#1#1#*\n*DE PRIMEIRA!*\n*1#1#1#1#1#1#*")
+        else:
+            print(u"Você teve %i erros\n"%pos)
+    else:
+        print (u'''
+  ___________.._______
+| .__________))______|
+| | / /      ||
+| |/ /       ||
+| | /        ||.-''.
+| |/         |/  _  \\
+| |          ||  x/x|
+| |          (\\\\`_.'
+| |         .-`--'.  *snap*
+| |        /Y . . Y\\
+| |       // |   | \\\\
+| |      //  | . |  \\\\
+| |     ')   |   |   (`
+| |          ||'||
+| |          || ||
+| |          || ||
+| |          || ||
+| |         / | | \\
+""""""""""|_`-' `-' |"""|
+|"|"""""""\ \   * * '"|"|
+| |        \ \*woosh* | |
+: :         \ \ * * * : :
+. .          `'       . .
+
+******************
+*   Que pena!    *
+* você falhou :( *
+******************
+            
+        ''')
+    return
+
+
+def fim(win):
+    global pos
+    if pos == 9 or win:
+        winPrint(win)
+        while True:
+            if win:
+                end = input("\n\nDeseja jogar novamente?(S/N)\n**********\n* BONUS! * - adicione ate 2 palavras ao jogo (A)\n**********\n")[0].upper()
+                if end=='A':
+                    addPalavra() #é lega chamar função dentro de outra assim em qqr linguagem ou só em algumas? Importaria eu declarar uma antes da outra em outra linguagem?
+                    end = input("\n\nDeseja jogar novamente?(S/N)\n")[0].upper() #tem uma maneira melhor de organizar os módulos?
+            else:
+                    end = input("\n\nDeseja jogar novamente?(S/N)\n")[0].upper()
+            if end=='N':
+                bye()
+            else:
+                print("\n\nVAMOS JOGAR NOVAMENTE!!\n\n\n\n\n") 
+                return True #fim = True
+    return False #fim = False
+
+
+def bye(): #atualizei aq pra poder sair do programa a "quaquer hora" e pq a funçao exit() n imprime bunitu assim
+    print(u'''
+******************************************            
+*░░░░░░░░░░░░░░░░░░░░░░█████████░░░░░░░░░*
+*░░███████░░░░░░░░░░███▒▒▒▒▒▒▒▒███░░░░░░░*
+*░░█▒▒▒▒▒▒█░░░░░░░███▒▒▒▒▒▒▒▒▒▒▒▒▒███░░░░*
+*░░░█▒▒▒▒▒▒█░░░░██▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒██░░*
+*░░░░█▒▒▒▒▒█░░░██▒▒▒▒▒██▒▒▒▒▒▒██▒▒▒▒▒███░*
+*░░░░░█▒▒▒█░░░█▒▒▒▒▒▒████▒▒▒▒████▒▒▒▒▒▒██*
+*░░░█████████████▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒██*
+*░░░█▒▒▒▒▒▒▒▒▒▒▒▒█▒▒▒▒▒▒▒▒▒█▒▒▒▒▒▒▒▒▒▒▒██*
+*░██▒▒▒▒▒▒▒▒▒▒▒▒▒█▒▒▒██▒▒▒▒▒▒▒▒▒▒██▒▒▒▒██*
+*██▒▒▒███████████▒▒▒▒▒██▒▒▒▒▒▒▒▒██▒▒▒▒▒██*
+*█▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒█▒▒▒▒▒▒████████▒▒▒▒▒▒▒██*
+*██▒▒▒▒▒▒▒▒▒▒▒▒▒▒█▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒██░*
+*░█▒▒▒███████████▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒██░░░*
+*░██▒▒▒▒▒▒▒▒▒▒████▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒█░░░░░*
+*░░████████████░░░█████████████████░░░░░░*
+******************************************
+        * Por hoje é só, pessoal *
+        **************************
+''')
+    exit()
                        
 
 #main
@@ -351,93 +449,11 @@ $R@i.~~ !     :   ~$$$$$B$$en:``
 while True:    
     forca=start()
     #if len(forca[0])<=2: #erro corrigido diretamente pela função escolhePalavra
-        #continue #erro de palavra vazia
+        #continue #erro de palavra vazia (corrigido ^)
     #print(forca[0]) #peep/cheat
     while True: #era while pos<8 mas a img tava ficando errada
         chute=chuta()
         forca[1]=checaChute(forca[0],chute,forca[1])
-        if pos==9: #tive que fazer pra acertar a imagem
+        if fim(bool('_' not in forca[1])): #olha como tava antes (commit: e74bae6556e79143a1ecf77f62decf98c13503b2)
             break
         geraImg(forca[1])
-        win = bool('_' not in forca[1])
-        if win:
-            palavras.remove(forca[0]) #retira reocorrencia de palavras
-            print (u'''
-
-            
-**************
-*  PARABÉNS  *
-*VOCÊ ACERTOU*
-**************
-            
-            ''')
-            break
-    if pos==0:
-            print("*1#1#1#1#1#1#*\n*DE PRIMEIRA!*\n*1#1#1#1#1#1#*")
-    else:
-        if not win:
-            print (u'''
-  ___________.._______
-| .__________))______|
-| | / /      ||
-| |/ /       ||
-| | /        ||.-''.
-| |/         |/  _  \\
-| |          ||  x/x|
-| |          (\\\\`_.'
-| |         .-`--'.  *snap*
-| |        /Y . . Y\\
-| |       // |   | \\\\
-| |      //  | . |  \\\\
-| |     ')   |   |   (`
-| |          ||'||
-| |          || ||
-| |          || ||
-| |          || ||
-| |         / | | \\
-""""""""""|_`-' `-' |"""|
-|"|"""""""\ \   * * '"|"|
-| |        \ \*woosh* | |
-: :         \ \ * * * : :
-. .          `'       . .
-
-******************
-*   Que pena!    *
-* você falhou :( *
-******************
-            
-        ''')
-        else: print(u"Você teve %i erros\n"%pos)
-    while True:#zoei o expediente aq... seria melhor fz uma função pra essa parte/como daria pra limpar?
-        if win:
-            end = input("\n\nDeseja jogar novamente?(S/N)\n**********\n* BONUS! * - adicione ate 2 palavras ao jogo (A)\n**********\n")[0].upper()
-            if end=='A':
-                addPalavra()
-                end = input("\n\nDeseja jogar novamente?(S/N)\n")[0].upper()
-        else:
-            end = input("\n\nDeseja jogar novamente?(S/N)\n")[0].upper()
-        if end=='N':
-            print(u'''
-******************************************            
-*░░░░░░░░░░░░░░░░░░░░░░█████████░░░░░░░░░*
-*░░███████░░░░░░░░░░███▒▒▒▒▒▒▒▒███░░░░░░░*
-*░░█▒▒▒▒▒▒█░░░░░░░███▒▒▒▒▒▒▒▒▒▒▒▒▒███░░░░*
-*░░░█▒▒▒▒▒▒█░░░░██▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒██░░*
-*░░░░█▒▒▒▒▒█░░░██▒▒▒▒▒██▒▒▒▒▒▒██▒▒▒▒▒███░*
-*░░░░░█▒▒▒█░░░█▒▒▒▒▒▒████▒▒▒▒████▒▒▒▒▒▒██*
-*░░░█████████████▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒██*
-*░░░█▒▒▒▒▒▒▒▒▒▒▒▒█▒▒▒▒▒▒▒▒▒█▒▒▒▒▒▒▒▒▒▒▒██*
-*░██▒▒▒▒▒▒▒▒▒▒▒▒▒█▒▒▒██▒▒▒▒▒▒▒▒▒▒██▒▒▒▒██*
-*██▒▒▒███████████▒▒▒▒▒██▒▒▒▒▒▒▒▒██▒▒▒▒▒██*
-*█▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒█▒▒▒▒▒▒████████▒▒▒▒▒▒▒██*
-*██▒▒▒▒▒▒▒▒▒▒▒▒▒▒█▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒██░*
-*░█▒▒▒███████████▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒██░░░*
-*░██▒▒▒▒▒▒▒▒▒▒████▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒█░░░░░*
-*░░████████████░░░█████████████████░░░░░░*
-******************************************
-        * Por hoje é só, pessoal *
-        **************************
-''')
-            exit()
-        print("\n\nVAMOS JOGAR NOVAMENTE!!\n\n\n\n\n")
-        break
